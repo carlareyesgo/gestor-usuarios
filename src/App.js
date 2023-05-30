c362315ffb6aa7a29d22beab28a0317581e28ec5
Carla Godínez <carlareyesgo@gmail.com>
2023-05-30 16:33:32 -0700

    Componente Card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import useFormulario from './hooks/useFormulario'
 import Input from './components/Input'
+import Card from './components/Card'
 
 function App() {
-  const [formulario, handleChange] = useFormulario({name: '', lastname: ''})
+  const [formulario, handleChange] = useFormulario({ name: '', lastname: '' })
 
   console.log(formulario)
   return (
-   <form>
-      <Input
-      name="name"
-      label="Nombre"
-      value={formulario.name}
-      onChange={handleChange}
-      />
-      <Input
-      name="lastname"
-      label="Apellido"
-      value={formulario.lastname}
-      onChange={handleChange}
-      />
-   </form>
+    <Card>
+      <form>
+        <Input
+          name="name"
+          label="Nombre"
+          value={formulario.name}
+          onChange={handleChange}
+        />
+        <Input
+          name="lastname"
+          label="Apellido"
+          value={formulario.lastname}
+          onChange={handleChange}
+        />
+      </form>
+    </Card>
   )
 }
 
